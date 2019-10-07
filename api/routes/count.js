0a1655f7f3586777e2a1408fabbb843624845c61
Harmon Singh <harmon.singh@my.jcu.edu.au>
2019-10-07 22:17:13 +1000

    setup database integration with postgresql

--- api/routes/count.js
+++ api/routes/count.js
@@ -3,26 +3,50 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const parkCount = require('../models/parkCount');
+const Pool = require('pg').Pool
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /count'
-    });
-});
+const pool = new Pool({
+    user: 'admin',
+    host: '127.0.0.1',
+    database: 'elements',
+    password: 'admin',
+    port: 2223
+})
+
+
+// router.get('/', (req, res, next) => {
+//     res.status(200).json({
+//         message: 'Handling GET requests to /count'
+//     });
+// });
 
 router.post('/:parkID', (req, res, next) => {
     const id = req.params.parkID;
-    const count = new parkCount({
-        name: id,
-        count: req.body.count
-    })
-    count.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'New number of cars posted'
+    const count = req.body.count;
+    // const count = new parkCount({
+    //     name: id,
+    //     count: req.body.count
+    // })
+    // count.save().then(result => {
+    //     console.log(result);
+    //     res.status(201).json({
+    //         message: 'New number of cars posted'
+    //     });
+    // })
+    //.catch(err => console.log(err));
+
+    pool.query('INSERT INTO iot.iot_node (node_id, node_name) VALUES ($1, $2)', [2, id], (error, res) => {
+        if (error) {
+          throw error
+        }
+        res.status(200).json({
+            message: 'Handling POST user added with ID: ${res.rows[0].id}'
         });
+        //res.status(201).send(`User added with ID: ${res.rows[0].id}`)
     })
-    .catch(err => console.log(err));
+    // res.status(200).json({
+    //     message: 'New number of cars posted'
+    // });
 });
 
 
